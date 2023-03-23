86e5a2a46be4cc714cf6a662a9f827dc62371203
Bhushan Pawar <113659167+bhushanpawr@users.noreply.github.com>
2023-03-23 09:08:33 +0530

    Create 01- Basic(station table).sql

---
+++ 01- Basic(station table).sql
@@ -0,0 +1,12 @@
+/*
+ ______________________________
+|    field     |   type        |
+| ID           | NUMBER        |
+| CITY         | VARCHAR(17)   |
+| STATE        | VARCHAR(3)    |
+| LAT_N        | VARCHAR(20)   |
+| lONG_W       | NUMBER        |
+ ______________________________
+*/
+# Q1) Query a list of CITY and STATE from the STATION table..
+SELECT CITY,STATE FROM STATION;
